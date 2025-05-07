2fce887d5427e4738ea728970c1c5d023da1854e
Heng <1014578764@outlook.com>
2025-05-08 03:51:34 +0900

    Create Main.cpp

---
+++ lesson10_Practice/Main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "Mutiply.h"
+
+
+int main()
+{
+	MutiplyAndLog (3 , 2);  //调用函数MutiplyAndLog三次
+	MutiplyAndLog (8 , 5);
+	MutiplyAndLog (32 , 27);
+	std::cin.get();
+}
