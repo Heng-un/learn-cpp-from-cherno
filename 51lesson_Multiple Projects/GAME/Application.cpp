bcdf4e5d1bcb22d980f43f754454b3bb864f510e
Heng <1014578764@outlook.com>
2025-07-21 04:51:50 +0900

    Delete 51lesson_Multiple Projects/GAME directory

--- 51lesson_Multiple Projects/GAME/Application.cpp
+++
@@ -1,9 +0,0 @@
-//#include ../Engine/src/Engine.h  //直接访问，这种情况文件有一些变动就可能出问题
-#include "Engine.h"  //设置GAME项目附加包含目录$(solutionDir)Engine\src
-#include <iostream>
-
-int main()
-{
-	engine::PrintMessage();
-	std::cin.get();
-}
